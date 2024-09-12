1669b8f94d713bb47f20012a897c3f9b648ffd4c
Ankit Gour <127835812+Ankit-Gour@users.noreply.github.com>
2024-09-12 16:14:18 +0530

    contact page

---
+++ index.js
@@ -0,0 +1,8 @@
+function register(){
+
+    console.log("clicked")
+    document.getElementById("about").outerHTML='';
+    document.getElementById("features").outerHTML='';
+    document.getElementById("how-it-works").outerHTML='';
+    document.getElementById("hero").outerHTML='';
+}
